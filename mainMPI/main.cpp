626d64e77f26af95532b35d00c2529dbee9c85fc
Federal <espada_2255@hotmail.com>
2023-06-02 18:49:44 -0700

    Lectura de vector

--- mainMPI/main.cpp
+++ mainMPI/main.cpp
@@ -1,23 +1,69 @@
 // archivo main.cpp
 #include <stdio.h>
 #include <stdlib.h>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <fstream>
 #include "mpi.h"
- 
+
+std::vector<int> leerVector(const std::string& filename);
+
 int main(int argc, char* argv[])
 {
     MPI_Init(&argc, &argv);
- 
-    // Check that the application is run with 4 processes.
+
+    // Condicion de los 4 procesos
     int size;
     MPI_Comm_size(MPI_COMM_WORLD, &size);
-    if(size != 4)
+    if (size != 4)
     {
-        printf("El parametro de ejecucion debe de ser 4\n");
+        printf("El parámetro de ejecución debe ser 4\n");
         MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
     }
- 
-    printf("bien hecho\n");
+
+    // Read vector from file
+    std::string filename = "vector.txt";  // Nombre del archivo
+    std::vector<int> vector = leerVector(filename);
+
+
+    printf("\nbien hecho\n");
+
     MPI_Finalize();
- 
+
     return EXIT_SUCCESS;
+}
+
+std::vector<int> leerVector(const std::string& filename)
+{
+    std::vector<int> vector;
+
+    std::ifstream file(filename);
+    if (file.is_open())
+    {
+        std::string line;
+        if (std::getline(file, line))
+        {
+            std::stringstream ss(line);
+            std::string number;
+            while (std::getline(ss, number, ';'))
+            {
+                vector.push_back(std::stoi(number));
+            }
+        }
+        file.close();
+    }
+    else
+    {
+        printf("No se pudo abrir el archivo '%s'\n", filename.c_str());
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    if (vector.size() % 4 != 0 || vector.size() <= 4)
+    {
+        printf("El largo del vector debe ser múltiplo de 4 y mayor que 4.\n");
+        MPI_Abort(MPI_COMM_WORLD, EXIT_FAILURE);
+    }
+
+    return vector;
 }
